c27df535570d383f97021b9ced2fa827124a431b
Tadi <tadilokesh8@gmail.com>
2025-04-30 10:38:03 +0530

    Your commit message

---
+++ server/api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('../routes/authRoutes');
+const blogRoutes = require('../routes/blogRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/blogs', blogRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+module.exports = app; // For Vercel
